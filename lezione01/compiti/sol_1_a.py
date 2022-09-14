1b1fae1244914f126c92c5a83bfba9899a10323a
Claudio Loi <claudio.loi92@gmail.com>
2022-09-14 21:35:47 +0200

    Completed first lesson homeworks with bonus

--- lezione01/compiti/sol_1_a.py
+++ lezione01/compiti/sol_1_a.py
@@ -1,41 +1,11 @@
-# Compito 1
-# Creare uno script di python che simuli il login ad un sito di casino.
-# - si chieda all'utente: nome, cognome, ed età 
+print("Welcome! Insert your informations.\n")
 
-print(" - - - Login del casino - - - \n\n")
+name = input("Insert your name: ")
+surname = input("Insert your surname: ")
+age = int(input("Insert your age: "))
 
-name = input("Inserisci il nome: ")
-surname = input("Inserisci il cognome: ")
-age = int(input("Inserisci l'etá: "))
-
-# - si verifichi che il nome e il cognome siano più lunghi di un carattere e che l'età sia maggiore o uguale di 18 anni
-# - se tutto è corretto stampare una string di conferma altrimenti comunicare l'errore all'utente
-# - si consideri che l'utente fornirà sempre dati di tipo corretto
-
-## V1 
-# following all the small steps to get to the solution for the name
-
-name_len = len(name)
-surname_len = len(surname)
-
-is_ok_name = name_len > 1
-is_ok_surname = surname_len > 1
-
-both_ok = is_ok_name and is_ok_surname 
-
-if both_ok:
-    print("Name and surname are correct")
-    print("Welcome in " + name.capitalize() + " " + surname.capitalize())
-else: 
-    print("Name or surname are too short")
-
-# now to the age part 
-
-f = age >= 18
-
-if f:
-    print(str(age) + " yo is good enough. You are a big person now! Go enjoy the world")
-else: 
-    print("I think u need to grow up a little")
-
-# *BONUS: stampare resoconto dei dati assicurandosi che nome e cognome inizino con la maiuscola*
+if len(name) > 1 and name[0].isupper() and len(surname) > 1 and surname[0].isupper() and age >= 18:
+    print("\nLogged in successfully!\n")
+    print(f"Your name is '{name}', your surname is '{surname}' and your age is '{age}'.")
+else:
+    print("Login failed. Some data are incorrect!")
